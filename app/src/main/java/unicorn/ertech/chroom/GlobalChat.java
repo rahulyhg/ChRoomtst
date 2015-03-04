1ca9d3e557f2ee9f5bb4019ab285e52631a22743
JawaJedi <tim-94a@yandex.ru>
2015-03-04 20:32:38 +0500

    33

--- app/src/main/java/unicorn/ertech/chroom/GlobalChat.java
+++ app/src/main/java/unicorn/ertech/chroom/GlobalChat.java
@@ -15,8 +15,6 @@ import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.util.Log;
 
-import com.astuetz.PagerSlidingTabStrip;
-
 import java.util.ArrayList;
 import java.util.List;
 
